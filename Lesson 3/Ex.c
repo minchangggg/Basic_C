21ffb202fa794dc9bb7f5e6d3866e8c94095e862
Minh Trang <125820144+minchangggg@users.noreply.github.com>
2023-11-15 12:35:28 +0700

    Update Ex.c

--- Lesson 3/Ex.c
+++ Lesson 3/Ex.c
@@ -2,27 +2,27 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <math.h>
-typedef int Toado[2];
+typedef int16_t Point[2];
 
-void inputCoordinates (Toado A) {
+void inputCoordinates (Point A) {
     printf ("\nNEnter Coordinates of Point");
-    printf ("\nEnter value of Abscissa: "); scanf ("%d",&A[0]);
-    printf ("Enter value of Ordinate: "); scanf ("%d",&A[1]);
+    printf ("\nEnter value of Abscissa: "); scanf ("%hd",&A[0]);
+    printf ("Enter value of Ordinate: "); scanf ("%hd",&A[1]);
 }
  
-float calcDistance ( Toado A, Toado B ) {
-    printf ("\nDistance between two points (%d,%d) and (%d,%d) is ", A[0],A[1],B[0],B[1]);
+float calcDistance ( Point A, Point B ) {
+    printf ("\nDistance between two points (%hd,%hd) and (%hd,%hd) is ", A[0],A[1],B[0],B[1]);
     printf ( "%.2f", sqrt ( pow((A[0]-B[0]),2) + pow((A[1]-B[1]),2) ) );
 }
 
-float calcAreaTriangle ( Toado A, Toado B, Toado C ) {
-    printf ("\nArea of triangle formed by three points (%d,%d), (%d,%d) and (%d,%d) is ", A[0],A[1],B[0],B[1],C[0],C[1]);
+float calcAreaTriangle ( Point A, Point B, Point C ) {
+    printf ("\nArea of triangle formed by three points (%hd,%hd), (%hd,%hd) and (%d,%d) is ", A[0],A[1],B[0],B[1],C[0],C[1]);
     // S = 0.5*|(xB-xA)(yC-yA)-(xC-xA)(yB-yA)|
     printf ( "%.2f", 0.5*abs((B[0]-A[0])*(C[1]-A[1])-(C[0]-A[0])*(B[1]-A[1])) );
 }
 
 int main () {
-    Toado A, B, C;
+    Point A, B, C;
     int choice_input;
     while (1) {
         do {
@@ -47,7 +47,4 @@ int main () {
             }
         } while (choice_input<0||choice_input>2);
     }
-    
 }
-
-
